fb5fa0f8198fc8b050b4ffd30e0bfbff2a15fce6
mengyangdy <466879168@qq.com>
2024-11-05 15:23:01 +0800

    chore(projects): release v0.2.6

--- src/constants/prettier.ts
+++ src/constants/prettier.ts
@@ -1,10 +1,15 @@
 import type { PartialPrettierExtendedOptions } from '../types';
 
 export const DEFAULT_PRETTIER_RULES: PartialPrettierExtendedOptions = {
-  printWidth: 120,
+  printWidth: 80,
+  tabWidth:2,
+  semi:false,
   singleQuote: true,
+  bracketSpacing:true,
+  bracketSameLine:false,
   trailingComma: 'none',
   arrowParens: 'avoid',
   htmlWhitespaceSensitivity: 'ignore',
-  jsdocCapitalizeDescription: false
+  jsdocCapitalizeDescription: false,
+  singleAttributePerLine:true
 };
